d180f13bbd62c1dc0dcbdfc35c57403926ad9f56
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2022-06-22 22:10:55 +0800

    vertical image slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 0) {
-        $('header').addClass('navbar-sticky');
-        $('.nav-content-target').addClass('nav-content-sticky').removeClass('nav-content');
-        $('.nav-login-target').addClass('nav-login-sticky').removeClass('nav-login');
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 0) {
+            $('header').addClass('navbar-sticky');
+            $('.nav-content-target').addClass('nav-content-sticky').removeClass('nav-content');
+            $('.nav-login-target').addClass('nav-login-sticky').removeClass('nav-login');
 
-    }
-    if ($(window).scrollTop() < 1) {
-        $('header').removeClass('navbar-sticky');
-        $('.nav-content-target').removeClass('nav-content-sticky').addClass('nav-content');
-        $('.nav-login-target').removeClass('nav-login-sticky').addClass('nav-login');
-    }
+        }
+        if ($(window).scrollTop() < 1) {
+            $('header').removeClass('navbar-sticky');
+            $('.nav-content-target').removeClass('nav-content-sticky').addClass('nav-content');
+            $('.nav-login-target').removeClass('nav-login-sticky').addClass('nav-login');
+        }
+    });
 });
